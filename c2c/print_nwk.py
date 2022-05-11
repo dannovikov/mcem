2c553bb447cf6a89329d17345fde3c63415d6fdf
Daniel <dnovikov@optonline.net>
2022-05-11 14:44:23 -0400

    Cleaning up files

--- c2c/print_nwk.py
+++
@@ -1,7 +0,0 @@
-import dendropy
-import sys
-
-nwk = sys.argv[1]
-taxa = dendropy.TaxonNamespace()
-tree = dendropy.Tree.get(path=nwk, schema='newick', taxon_namespace=taxa, preserve_underscores=True, suppress_edge_lengths=True)
-tree.print_plot()
